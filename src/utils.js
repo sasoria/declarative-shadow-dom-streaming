5e2f3532ca677032f90f950ec2d40c443d6a7b46
sasoria <sergio.alberto.arevalo.soria@nav.no>
2024-05-12 15:29:33 +0200

    Add fetch to api

--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,4 @@
-import { render, html } from 'swtl';
+import { render, html } from "swtl";
 
 export const globalStyles = () => html`
   <style>
@@ -6,27 +6,11 @@ export const globalStyles = () => html`
       --color-primary: #2d3748;
       --color-secondary: #ffc60d;
     }
-
-    body {
-      font-family: system-ui, sans-serif;
-      margin: 0;
-      color: var(--color-primary);
-    }
-
-    a {
-      color: var(--color-primary);
-    }
   </style>
 `;
 
 export const appStyles = () => html`
   <style>
-    :host {
-      display: flex;
-      flex-direction: column;
-      min-height: 100vh;
-    }
-
     header,
     main,
     footer {
@@ -43,16 +27,6 @@ export const appStyles = () => html`
       h1 {
         margin: 0;
         font-size: 1.5rem;
-      }
-    }
-
-    main {
-      flex-grow: 1;
-    }
-
-    footer {
-      a {
-        color: var(--color-secondary);
       }
     }
   </style>
@@ -75,33 +49,4 @@ export function createReadableStreamFromAsyncGenerator(output) {
       }
     },
   });
-}
-
-export const delayed = (ms, data) =>
-  new Promise((resolve) => {
-    setTimeout(() => {
-      resolve(data);
-    }, ms);
-  });
-
-export async function* renderInResolvedOrder(promises) {
-  let promisesWithIndexes = promises.map((promise, index) => {
-    return {
-      index,
-      promise: promise.then((value) => {
-        return { index, value };
-      }),
-    };
-  });
-
-  while (promisesWithIndexes.length > 0) {
-    const result = await Promise.race(
-      promisesWithIndexes.map((p) => p.promise)
-    );
-    yield* render(result.value);
-
-    promisesWithIndexes = promisesWithIndexes.filter((promise) => {
-      return promise.index !== result.index;
-    });
-  }
 }
